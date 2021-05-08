2319ccdc290d6b7e11ab3ef737099052c7c47081
hirofumi_i <detroit69city.nau@gmail.com>
2021-05-08 16:17:00 +0900

    Update pagination component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,50 +1,23 @@
 import Link from 'next/link';
 
 interface Props {
-  prev: number;
-  next: number;
-  current: number;
-  total: number;
+  prev: string | null;
+  next: string | null;
 }
 
 export default function Pagination(props: Props) {
-  const { prev, next, current, total } = props;
+  const { prev, next } = props;
 
-  const Prev = () => {
-    return prev > 1 ? (
-      <Link
-        href={{
-          pathname: '/page/[number]',
-          query: {
-            prev: `${prev - 1}`,
-            next: current,
-          },
-        }}
-        as={`/page/${prev}`}
-      >
-        <a className="pagination__prev">Prev</a>
-      </Link>
-    ) : (
-      <Link href="/">
-        <a className="pagination__prev">Prev</a>
-      </Link>
-    );
-  };
   return (
     <nav className="pagination">
-      {prev > 0 && <Prev />}
+      {prev ? (
+        <Link href={prev}>
+          <a className="pagination__prev">Prev</a>
+        </Link>
+      ) : null}
 
-      {next <= total && (
-        <Link
-          href={{
-            pathname: '/page/[number]',
-            query: {
-              prev: current,
-              next: `${next + 1}`,
-            },
-          }}
-          as={`/page/${next}`}
-        >
+      {next && (
+        <Link href={next}>
           <a className="pagination__next">Next</a>
         </Link>
       )}

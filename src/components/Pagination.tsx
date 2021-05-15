52e818f468391c58cd822efd6f078387974a096c
hirofumi_i <detroit69city.nau@gmail.com>
2021-05-15 18:26:55 +0900

    Update pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { styled } from 'styles/config';
 
 interface Props {
   prev: string | null;
@@ -9,18 +10,57 @@ export default function Pagination(props: Props) {
   const { prev, next } = props;
 
   return (
-    <nav className="pagination">
+    <Wrapper>
       {prev ? (
-        <Link href={prev}>
-          <a className="pagination__prev">Prev</a>
+        <Link href={prev} passHref>
+          <PaginationPrev>Prev</PaginationPrev>
         </Link>
       ) : null}
 
       {next && (
-        <Link href={next}>
-          <a className="pagination__next">Next</a>
+        <Link href={next} passHref>
+          <PaginationNext className="pagination__next">Next</PaginationNext>
         </Link>
       )}
-    </nav>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled('nav', {
+  fontSize: '1.4rem',
+  display: 'flex',
+  margin: '3rem 0',
+  justifyContent: 'space-between',
+
+  '& a': {
+    fontWeight: '700',
+    display: 'inline-block',
+    padding: '1rem 1.1rem',
+    border: '1px solid $primary',
+    '&:hover': {
+      textDecoration: 'none',
+      color: '#fff',
+      backgroundColor: '$primary',
+    },
+  },
+});
+
+const PaginationPrev = styled('a', {
+  marginRight: 'auto',
+  '&::before': {
+    display: 'inline-block',
+    marginRight: '0.5rem',
+    content: '«',
+    textAlign: 'center',
+  },
+});
+
+const PaginationNext = styled('a', {
+  marginLeft: 'auto',
+  '&::after': {
+    display: 'inline-block',
+    marginRight: '0.5rem',
+    content: '»',
+    textAlign: 'center',
+  },
+});

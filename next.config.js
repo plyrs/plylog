be20c6756363d73b916998b8b2dd7bf99ff1eb2d
hirofumi_i <detroit69city.nau@gmail.com>
2021-05-16 11:20:46 +0900

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = {
+  reactStrictMode: true,
+  poweredByHeader: false,
   webpack: config => {
     config.module.rules.push({
       test: /\.md$/,
